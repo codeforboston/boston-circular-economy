46930d9a0aa796c1b8ce86e08e724a36eae56e2c
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2026-03-08 10:14:55 -0400

    Use new location for tanstackRouter vite plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/boston-circular-economy/',
   plugins: [
-    TanStackRouterVite({ routesDirectory: './src/pages' }),
+    tanstackRouter({ routesDirectory: './src/pages' }),
     react(),
   ],
 })
